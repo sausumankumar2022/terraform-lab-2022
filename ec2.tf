a08276e2fb18f7bf086f65bb2c9634233e5a0ea0
suman kumar sau <92514449+sausumankumar2022@users.noreply.github.com>
2023-03-03 15:19:18 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0f51057b3256c013a"
+  ami           = "ami-006dcf34c09e50022"
   instance_type = "t2.micro"
   availability_zone = "us-east-1a"
   tags = {
