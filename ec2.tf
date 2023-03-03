6b03740a7357cd2c5621419fac866cf3c4a71c1a
jagdishmodi <48287274+jagdishmodi@users.noreply.github.com>
2023-03-03 14:16:45 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,22 +1,6 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["amazon"] # Canonical
-}
-
 resource "aws_instance" "web" {
   ami           = "ami-0f51057b3256c013a"
-  instance_type = "t3.xlarge"
+  instance_type = "t2.micro"
   availability_zone = "us-east-1a"
   tags = {
     Name = "HelloWorld-dev"
